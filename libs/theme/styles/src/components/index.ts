c9751e06bf037df903b11ef743e17a1933a24842
Aaron Miller <drkstr101@gmail.com>
2023-07-02 22:21:33 +0000

    add text style component

--- libs/theme/styles/src/components/index.ts
+++ libs/theme/styles/src/components/index.ts
@@ -9,3 +9,7 @@ export type { ContainerTheme, ContainerVariant } from "./container";
 // Heading
 export { default as heading } from "./heading";
 export type { HeadingTheme, HeadingVariant } from "./heading";
+
+// Text
+export { default as text } from "./text";
+export type { TextTheme, TextVariant } from "./text";
