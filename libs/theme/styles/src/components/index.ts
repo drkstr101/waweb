06f705fc2ba45b874d0ec10ceca5afb1b46b4910
Aaron Miller <drkstr101@gmail.com>
2023-07-02 22:10:26 +0000

    add heading style component

--- libs/theme/styles/src/components/index.ts
+++ libs/theme/styles/src/components/index.ts
@@ -5,3 +5,7 @@ export type { ButtonColor, ButtonTheme, ButtonVariant } from "./button";
 // Container
 export { default as container } from "./container";
 export type { ContainerTheme, ContainerVariant } from "./container";
+
+// Heading
+export { default as heading } from "./heading";
+export type { HeadingTheme, HeadingVariant } from "./heading";
