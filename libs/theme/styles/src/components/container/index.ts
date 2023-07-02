85d2ebf642edc5931d956e56f42807caf75f7d5c
Aaron Miller <drkstr101@gmail.com>
2023-07-02 17:35:22 +0000

    update style components

--- libs/theme/styles/src/components/container/index.ts
+++ libs/theme/styles/src/components/container/index.ts
@@ -3,5 +3,7 @@ import styles from "./container.module.css";
 const { fixed, narrow, wide } = styles;
 const container = { fixed, narrow, wide } as const;
 
-export type ContainerStyles = typeof container;
-export default container;
+export type ContainerVariant = "fixed" | "narrow" | "wide";
+export type ContainerTheme = typeof container;
+
+export default container as ContainerTheme;
--- libs/theme/styles/src/components/index.ts
+++ libs/theme/styles/src/components/index.ts
@@ -1,2 +1,7 @@
+// Button
+export { default as button } from "./button";
+export type { ButtonColor, ButtonTheme, ButtonVariant } from "./button";
+
+// Container
 export { default as container } from "./container";
-export type { ContainerStyles } from "./container";
+export type { ContainerTheme, ContainerVariant } from "./container";
