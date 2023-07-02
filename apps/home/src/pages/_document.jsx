9b68919179525afb444fca9131e674339820f787
Aaron Miller <drkstr101@gmail.com>
2023-07-02 18:33:14 +0000

    update default font feature settings

--- apps/home/src/pages/_document.jsx
+++ apps/home/src/pages/_document.jsx
@@ -5,7 +5,7 @@ export default function Document(props) {
 
   return (
     <Html
-      className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'ss01']"
+      className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'ss03']"
       lang="en"
     >
       <Head>
